8c22810c904fd0785d7907eb0cfd91892ea0ad37
Jose Baerga Full Sail <jbaerga@fullsail.edu>
2015-09-06 00:42:31 -0500

    Promoted User for name
    Comment user Hello line on week2.js

--- GO To Training Week2/js/week2.js
+++ GO To Training Week2/js/week2.js
@@ -7,3 +7,13 @@ GoTo Training Week #2
 
 
 //alert("Test to see if connected")
+
+//Create an age calculator
+
+//Ask the user for their name
+var name= prompt("Let's figure out how old you " +
+    "are !\nPlease type in your name.");
+
+//Use the variable and sya hello
+alert("Welcome "+name+"!");
+
