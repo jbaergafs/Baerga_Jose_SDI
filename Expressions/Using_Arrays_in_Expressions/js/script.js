28cf2f108ab144c17848f1a53270b50bbc38fd1b
Jose Baerga Full Sail <jbaerga@fullsail.edu>
2015-09-10 14:03:19 -0500

    Expressions_Using_Arrays_in_Expressions sample
    Expressions_Using_Arrays_in_Expressions sample files

---
+++ Expressions/Using_Arrays_in_Expressions/js/script.js
@@ -0,0 +1,11 @@
+/**
+ * Created by josebaerga on 9/10/15.
+ */
+//Expressions - Using Arrays in Expressions
+
+var orangeBins = [234, 567, 883];
+
+
+var total = orangeBins[0] + orangeBins[1] + orangeBins[2];
+console.log(total);
+
