569df5b51d77fcfecb82709a319cbfa9175e5704
Jose Baerga Full Sail <jbaerga@fullsail.edu>
2015-09-10 14:17:21 -0500

    Expressions_Concatenating sample
    Expressions_Concatenating sample

---
+++ Expressions/Concatenating/js/script.js
@@ -0,0 +1,16 @@
+/**
+ * Created by josebaerga on 9/10/15.
+ */
+//Expressions - Concatenating
+
+var firstName = "Jose";
+var lastName = "Baerga";
+var fullName = firstName + " " + lastName;
+
+console.log(fullName);
+
+var a = "6";
+var b = "7";
+
+var result = a + b;
+console.log(result);
