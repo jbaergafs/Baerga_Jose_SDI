bb89717d8a84825b34c05aa7ef44618f941c766d
Jose Baerga Full Sail <jbaerga@fullsail.edu>
2015-09-10 15:06:00 -0500

    Prompt_and_Alert sample
    Prompt_and_Alert sample

---
+++ Prompt and Alert/js/script.js
@@ -0,0 +1,19 @@
+/**
+ * Created by josebaerga on 9/10/15.
+ */
+//Prompt and Alert
+
+// Enter yearBorn variable
+
+var yearBorn = prompt("Enter your year of birth");
+console.log(yearBorn);
+
+//Variable of widht and height
+
+var width = prompt("We are calculating the are of a rectangle. \n Please anter the width");
+var height = prompt("Please enter the height");
+var area = width * height; // calculates with that info
+//console.log(area); //prints it out to console
+var result = alert("The area of your rectangle is " + area + "sq feet");
+alert(result);
+
