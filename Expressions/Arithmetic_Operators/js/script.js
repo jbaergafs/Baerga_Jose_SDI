7e6a6b05c48a8977609220927a92baf2c4617437
Jose Baerga Full Sail <jbaerga@fullsail.edu>
2015-09-10 12:27:31 -0500

    Expression-Arithmetic_Operators sample
    Expression-Arithmetic_Operators sample

---
+++ Expressions/Arithmetic_Operators/js/script.js
@@ -0,0 +1,14 @@
+/**
+ * Created by josebaerga on 9/10/15.
+ */
+//Expressions - Arithmetic Operators
+
+// area of the triangle is half of the with time the height
+
+var width = 7;
+var height = 5;
+var area = width * height/2;
+console.log(area);
+
+
+
