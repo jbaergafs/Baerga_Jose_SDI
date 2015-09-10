be64375e7f12ae8ff97dfc2f56b72902c9a3c382
Jose Baerga Full Sail <jbaerga@fullsail.edu>
2015-09-10 15:53:03 -0500

    Expression_Worksheet_Circle-Area
    Expression_Worksheet_Circle-Area
    
    Worksheet # 1 Circle A = Pi * r *2

---
+++ Expressions_worksheet/Circle_area/js/script.js
@@ -0,0 +1,32 @@
+/**
+ * Created by josebaerga on 9/10/15.
+ * SDI Class Section 01
+ * Week 2 Assignment - Expression _worksheet
+ * instructor: Eric Silvay
+ */
+
+//Expressions_worksheet - Calculate the are of a circle given the radius of a circle.
+
+//Variables of calculating the Circumference oaf a Circle.
+
+//entering the radius of the circle.
+
+var radius = prompt("We are calculating the area of a circle \n Pleas enter the radius of the circle");
+console.log(radius);
+
+//The constant value of Pi.
+
+var constPi = 3.14159265;
+
+//multiply 2, Pi and the radius & assign to cirArea variable.
+
+var cirArea = radius * 2 * constPi;
+
+//print out the result of the circumference area.
+
+console.log("The circumference area of this circle is" + " " + cirArea); //print on console the results of circumference area.
+alert("The circumference area of this circle is" + " " + cirArea); // Prompt the message of the circumference area.
+
+
+
+
