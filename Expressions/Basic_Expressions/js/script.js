a3a28175f1ea4107232f78150f13b26d56f932e0
Jose Baerga Full Sail <jbaerga@fullsail.edu>
2015-09-10 12:01:46 -0500

    Basic Expression Sample
    Basic Expression Sample

---
+++ Expressions/Basic_Expressions/js/script.js
@@ -0,0 +1,10 @@
+/**
+ * Created by josebaerga on 9/10/15.
+ */
+//Bassic Expression sample
+
+var a = 2;//setup our variable a and defines it with 2
+var b;//setup variable b
+var b = a + 3;//add 3 to a to difine value of b
+
+console.log(b);//console.log of the resaults of variable b
