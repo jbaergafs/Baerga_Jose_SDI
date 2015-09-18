fce04570ae425a14fbfe56b967461b58349c3f7f
Jose Baerga Full Sail <jbaerga@fullsail.edu>
2015-09-17 21:32:22 -0500

    Baerga_Jose_Conditional_Assigment first test
    Baerga_Jose_Conditional_Assigment first test

---
+++ Baerga_Jose_Conditionals_Assignment/js/script.js
@@ -0,0 +1,33 @@
+
+ /*
+Jose Baerga
+SDI Section #1
+Title _ Baerga JOse_Conditional_Assignment
+Date - 09/17/2015
+ */
+
+ //We are going to distribute commercial trucks on the highway truck stop by line and
+ // by weight, Maximum weight is 200,000 ponds and minimum is 175 pound
+ // Line # 1 is for truck equal or over 200,000 pounds , Line # 2 is for truck equal or less than 175,000.
+
+// lets found out how much is the trucks weight
+
+//variable of truckWeight
+
+ var truckWeigth = ["truck1" == 250000, "truck2" == 165000, "truck3" == 200000, "truck 4" == 100,000];
+
+ //Let see the print on console
+
+ console.log(truckWeigth);
+
+ //Variable of maxWeight = 200,000   minWeight = 175,000
+
+ var maxWeight = 200000;
+ var minWeight = 175000;
+
+ // Condition statement
+
+ if(truckWeigth => maxWeight;){
+       //print in console what truck is going to lane 1
+      console.log("Please go to lane # 1" + "" +truckWeigth+);
+ }
